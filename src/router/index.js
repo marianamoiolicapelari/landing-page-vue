16b73d0f02810ba913c6497a25966b3072d025c0
Mariana M.C <marianamoioli@gmail.com>
2022-11-16 09:43:29 -0300

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/pedidos',
+    name: 'Pedidos',
+    component: () => import(/* webpackChunkName: "pedidos" */ '../views/Pedidos.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+<Banner />
+  <div class="main-container">
+    <h1>Monte o seu burger:</h1>
+    <BurgerForm />
+  </div>
+</template>
+
+<script>
+import Banner from '../components/Banner.vue'
+import BurgerForm from '../components/BurgerForm.vue'
+export default {
+  name: 'Home',
+  components: {
+    Banner,
+    BurgerForm
+  }
+};
+</script>
